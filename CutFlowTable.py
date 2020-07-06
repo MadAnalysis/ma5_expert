ab3d0fe41994ca7055684c499e7c1261feaab050
jackaraz <jackaraz@gmail.com>
2020-07-06 11:18:35 +0100

    Update CutFlowTable.py

--- CutFlowTable.py
+++ CutFlowTable.py
@@ -41,10 +41,12 @@ class CutFlowTable:
         samples.remove(self.ref_sample)
         self.sample_names.remove(self.ref_name)
         self.samples = samples
-    
+
+
     def _sorter(self,x):
         return self.ref_sample[x].get_final_cut().Nentries
-    
+
+
     def write_comparison_table(self,*args,**kwargs):
         """
         Parameters
@@ -56,6 +58,8 @@ class CutFlowTable:
             only_alive : BOOLEAN (default True)
                 only write the SRs which has more than zero yield for reference
                 collection.
+            make : BOOL
+                Write the Makefile -> (default, True)
         Returns
         -------
         LaTeX tables of signal regions.
@@ -67,8 +71,8 @@ class CutFlowTable:
         file = None
         if len(args) > 0:
             file = args[0]
-            file.write(r'\documentclass[11pt]{article}'+'\n'+\
-                       r'\usepackage{pdflscape}'+'\n'+\
+            file.write(r'\documentclass[12pt]{article}'+'\n'+\
+                       r'\usepackage{pdflscape,slashed}'+'\n'+\
                        r'\begin{document}'+'\n'+\
                        r'\begin{landscape}'+'\n\n\n\n')
         for SR in SR_list:
@@ -90,7 +94,7 @@ class CutFlowTable:
                     txt += '\\\ \\hline\\hline\n'
             txt +='      & Events & $\\varepsilon$ &'
             for smp in self.sample_names:
-                txt += 'Events & $\\varepsilon$ & $\\delta$ [\%]'
+                txt += 'Events & $\\varepsilon$ & $|\\delta|$ [\%]'
                 if not self.sample_names.index(smp) == len(self.sample_names)-1:
                     txt += ' & '
                 else:
@@ -113,7 +117,7 @@ class CutFlowTable:
                     elif cutID > 0 and cut.rel_eff == 0:
                         txt += '{:.1f} & {:.3f} & - '.format(smp[cutID].Nevents,smp[cutID].rel_eff)
                     else:
-                        rel_eff =1-(smp[cutID].rel_eff/cut.rel_eff)
+                        rel_eff =abs(1-(smp[cutID].rel_eff/cut.rel_eff))
                         txt += '{:.1f} & {:.3f} & {:.1f}\\% '.format(smp[cutID].Nevents,smp[cutID].rel_eff,rel_eff*100.)
                     if smp != self.samples[-1][SR]:
                         txt += ' & '  
@@ -134,8 +138,10 @@ class CutFlowTable:
                 print(txt)
         if file != None:
             file.write('\n\n\n\n'+r'\end{landscape}'+'\n'+r'\end{document}'+'\n')
-            self.WriteMake(file,make=kwargs.get('make',True))
-    
+            if kwargs.get('make',True):
+                self.WriteMake(file,make=kwargs.get('make',True))
+
+
     def write_signal_comparison_table(self,*args,**kwargs):
         """
 
@@ -150,6 +156,12 @@ class CutFlowTable:
             only_alive : BOOLEAN (default True)
                 only write the SRs which has more than zero yield for reference
                 collection.
+            sys_sig : BOOL
+                Calculate S/sqrt(B+(B*sys)^2) -> (default False)
+            ZA : BOOL
+                Calculate Assimov significance -> (default False)
+            make : BOOL
+                Write the Makefile -> (default, True)
         Returns
         -------
         Signal over Background comparison table.
@@ -163,6 +175,10 @@ class CutFlowTable:
         file = None
         if len(args) > 0:
             file = args[0]
+            file.write(r'\documentclass[12pt]{article}'+'\n'+\
+                       r'\usepackage{pdflscape,slashed}'+'\n'+\
+                       r'\begin{document}'+'\n'+\
+                       r'\begin{landscape}'+'\n\n\n\n')
         for SR in SR_list:
             txt = '\n\n%% '+SR+'\n\n'
             txt+='\\begin{table}[h]\n'
@@ -194,18 +210,18 @@ class CutFlowTable:
                     name = name.replace('_',' ')
                 txt += '      '+name.ljust(40,' ') + '& '
                 if cutID == 0:
-                    txt += '{:.1f} & - &'.format(cut.Nevents,cut.rel_eff*100.)
+                    txt += '{:.1f} & - &'.format(cut.Nevents)
                 else:
-                    txt += '{:.1f} & {:.1f}\\% &'.format(cut.Nevents,cut.rel_eff*100.)
+                    txt += '{:.1f} & {:.3f} &'.format(cut.Nevents,cut.rel_eff)
                 
                 for sample in self.samples:
                     smp = sample[SR]
                     if cutID == 0:
-                        txt += '{:.1f} & - '.format(smp[cutID].Nevents,smp[cutID].rel_eff*100.)
+                        txt += '{:.1f} & - '.format(smp[cutID].Nevents)
                     elif cutID > 0 and cut.rel_eff == 0:
-                        txt += '{:.1f} & {:.1f}\\% '.format(smp[cutID].Nevents,smp[cutID].rel_eff*100.)
+                        txt += '{:.1f} & {:.3f} '.format(smp[cutID].Nevents,smp[cutID].rel_eff)
                     else:
-                        txt += '{:.1f} & {:.1f}\\% '.format(smp[cutID].Nevents,smp[cutID].rel_eff*100.)
+                        txt += '{:.1f} & {:.3f} '.format(smp[cutID].Nevents,smp[cutID].rel_eff)
                     if smp != self.samples[-1][SR]:
                         txt += ' & '  
                     else:
@@ -242,26 +258,28 @@ class CutFlowTable:
                             txt += ' & ' 
                         else:
                             txt += r'\\'
-                    
-                    txt += '\n     \\multicolumn{3}{c}{$S/\sqrt{B+(B\Delta_{sys})^2}$}  &'
-                    for sample in self.samples:
-                        smp = sample[SR]
-                        txt += '\\multicolumn{2}{c}{'+\
-                                '{:.3f}'.format(fom.sig_sys)+'}'
-                        if smp != self.samples[-1][SR]:
-                            txt += ' & ' 
-                        else:
-                            txt += r'\\'
-                            
-                    txt += '\n     \\multicolumn{3}{c}{$Z_A$} &'
-                    for sample in self.samples:
-                        smp = sample[SR]
-                        txt += '\\multicolumn{2}{c}{'+\
-                                '${:.3f} \\pm {:.3f} $'.format(fom.ZA,fom.ZA_err)+'}'
-                        if smp != self.samples[-1][SR]:
-                            txt += ' & ' 
-                        else:
-                            txt += r'\\'
+
+                    if kwargs.get('sig_sys',False):
+                        txt += '\n     \\multicolumn{3}{c}{$S/\sqrt{B+(B\Delta_{sys})^2}$}  &'
+                        for sample in self.samples:
+                            smp = sample[SR]
+                            txt += '\\multicolumn{2}{c}{'+\
+                                    '{:.3f}'.format(fom.sig_sys)+'}'
+                            if smp != self.samples[-1][SR]:
+                                txt += ' & ' 
+                            else:
+                                txt += r'\\'
+
+                    if kwargs.get('ZA',False):
+                        txt += '\n     \\multicolumn{3}{c}{$Z_A$} &'
+                        for sample in self.samples:
+                            smp = sample[SR]
+                            txt += '\\multicolumn{2}{c}{'+\
+                                    '${:.3f} \\pm {:.3f} $'.format(fom.ZA,fom.ZA_err)+'}'
+                            if smp != self.samples[-1][SR]:
+                                txt += ' & ' 
+                            else:
+                                txt += r'\\'
                     
                 txt += '\n'
 
@@ -274,8 +292,15 @@ class CutFlowTable:
                 file.write(txt)
             else:
                 print(txt)
+        if file != None:
+            file.write('\n\n\n\n'+r'\end{landscape}'+'\n'+r'\end{document}'+'\n')
+            if kwargs.get('make',True):
+                self.WriteMake(file,make=kwargs.get('make',True))
+
 
     def WriteMake(self,file,make=True):
+        if not file.name.endswith('.tex'):
+            raise ValueError('Input does not have .tex extention.')
         if os.path.isfile(file.name):
             make = open('Makefile','w')
             make.write('all:\n')
@@ -292,67 +317,3 @@ class CutFlowTable:
                     print 'Compilation failed.'
         else:
             raise ValueError('Can not find '+file.name)
-
-
-
-
-def Experimental_CutFlow(SR,SR_name,cut_flow):
-    if len(SR_name) != len(cut_flow):
-        print 'Size does not match: ', len(SR_name) , len(cut_flow)
-        return False
-    for i in range(len(cut_flow)):
-        if i == 0:
-            current_cut = cut(Name=SR_name[i], xsec=cut_flow[i])
-            cut_0       = current_cut
-            precut      = current_cut
-        else:
-            current_cut = cut(Name=SR_name[i], precut=precut,cut_0=cut_0, xsec=cut_flow[i])
-            precut      = current_cut
-        SR.add_cut(current_cut)
-    return SR
-
-def compare(*args,**kwargs):
-    SR_coll = []
-    for item in args:
-        if 'type' in item.__dict__.keys():
-            if item.type == 'SR_collection':
-                SR_coll.append(item)
-
-    colored = True
-    SR      = None
-    if 'colored' in kwargs.keys():
-        colored = kwargs['colored']
-    if 'SR' in kwargs.keys():
-        SR = kwargs['SR']
-    
-    #sr_name_size = {}
-    #for SR in SR_coll[0].keys():
-    #    sr_name_size[SR] = max([len(x.Name)+2 for x in SR_coll[0][SR].get_names()])
-    #    print ''.ljust(sr_name_size[SR], '='),SR_coll[0][SR].name
-    #    for inpt in SR_coll:
-    #    print ''.ljust(sr_name_size[SR], ' ')+
-    
-    
-    sr_list = list(args)
-    print ''.ljust(54, '='),sr_list[0].name
-    size = len(sr_list[0])
-    print ''.ljust(55, ' ') + 'ATLAS'.ljust(15, ' ') + ' | '+'MA5'.ljust(15, ' ') + ' | '+'MA5/ATLAS'.ljust(8, ' ') + ' | '
-    #print ''.ljust(55, ' ') + '------'.ljust(6, ' ') + ' | '+'------'.ljust(6, ' ') + ' | '+'--------'.ljust(8, ' ') + ' | '
-
-    for cut in range(size):
-        txt = ''
-        if cut%2 == 0 and colored:
-            txt += u'\u001b[31m'
-        txt += sr_list[0].get_cut(cut).Name.ljust(55, ' ')
-        for sr in range(len(sr_list)):
-            txt += str(round(sr_list[sr][cut].Nevents,1)).ljust(6, ' ') + ' | '
-            txt += str(round(sr_list[sr][cut].rel_eff,3)).ljust(6, ' ') + ' | '
-            if sr>0 and sr_list[0][cut].Nevents>0:
-                txt += str(round(sr_list[1][cut].Nevents/\
-                                 sr_list[0][cut].Nevents,3)).ljust(9, ' ') + ' | '
-            #else:
-                #txt += ' '.ljust(9, ' ') + ' | '
-        if cut % 2 == 0 and colored:
-            txt += u' \u001b[0m' 
-        print txt
-    print 
