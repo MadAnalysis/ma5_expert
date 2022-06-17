4f2896951d9a5f0f91dc33e2c8057d02d6270e42
jackaraz <jackaraz@gmail.com>
2022-06-17 17:26:36 +0100

    improve logger and make exceptions accessible

--- docs/releases/changelog-dev.md
+++ docs/releases/changelog-dev.md
@@ -5,9 +5,13 @@
     through MadAnalysis interface ([#7](https://github.com/MadAnalysis/ma5_expert/pull/7)).
 
 ## Improvements
- 
+  * Improve the logger output
+    ([#7](https://github.com/MadAnalysis/ma5_expert/pull/7)).
+
 ## Bug fixes
- 
+ * Make exceptions accessible through `ma5.system` module
+    ([#7](https://github.com/MadAnalysis/ma5_expert/pull/7)).
+
 ## Contributors
 
 This release contains contributions from (in alphabetical order):
--- src/ma5_expert/system/logger.py
+++ src/ma5_expert/system/logger.py
@@ -7,16 +7,17 @@ class ColoredFormatter(logging.Formatter):
         logging.Formatter.__init__(self, msg)
 
     def format(self, record):
-        if record.levelno >= 50:  # FATAL
-            color = "\x1b[31m Ma5 Expert - ERROR: "
-        elif record.levelno >= 40:  # ERROR
-            color = "\x1b[31m Ma5 Expert - ERROR: "
+        if record.levelno >= 40:  # ERROR
+            color = f"\x1b[31m Ma5 Expert - ERROR ({record.module}.{record.funcName}() " \
+                    f"in {record.filename}::L{record.lineno}): "
         elif record.levelno >= 30:  # WARNING
-            color = "\x1b[35m Ma5 Expert - WARNING: "
+            color = f"\x1b[35m Ma5 Expert - WARNING ({record.module}.{record.funcName}() " \
+                    f"in {record.filename}::L{record.lineno}): "
         elif record.levelno >= 20:  # INFO
             color = "\x1b[0m Ma5 Expert: "
         elif record.levelno >= 10:  # DEBUG
-            color = "\x1b[36m Ma5 Expert - DEBUG: "
+            color = f"\x1b[36m Ma5 Expert - DEBUG ({record.module}.{record.funcName}() " \
+                    f"in {record.filename}::L{record.lineno}): "
         else:  # ANYTHING ELSE
             color = "\x1b[0m Ma5 Expert: "
 
